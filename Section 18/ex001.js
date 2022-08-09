c6b2c143783e1b44498f4ff15a3e4fb1c6d676fd
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-08-09 15:29:53 -0300

    Create Section 18

---
+++ Section 18/ex001.js
@@ -0,0 +1,10 @@
+const student = {
+    firstName: "Maria",
+    lastName: "Boabaid",
+    subject: ['math', 'history'],
+    exams: { 
+        midterm: 100,
+        final: 80
+    }
+}
+console.log(student.exams.midterm)
---
+++ Section 18/ex002.js
@@ -0,0 +1,14 @@
+const shoppingCart = [
+    {
+        product: "Oreo",
+        price: 10,
+        quantity: 1
+    },
+    {
+        product: "Butter",
+        price: 5,
+        quantity: 2
+    }
+]
+
+console.log(shoppingCart[1])

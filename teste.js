5d442d1d0b4a43b52804ad3dc1ee1bc031c80bb9
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-08-15 10:36:10 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,4 +1,18 @@
-const greet = (name) => {
-    return console.log(`Hey ${name}`);
+const movies = [
+    { title: "Harry Potter", score: 10 },
+    { title: "Nothing Hill", score: 9 },
+    { title: "Percy Jackson", score: 5 },
+    { title: "Bambi", score: 7 }
+]
+
+const goodMovies = movies.filter((movie) => (
+    movie.score > 8));
+
+console.log(goodMovies)
+
+const greatMovies = function bestMovies(movie) {
+    return movie.filter(function filteringMovies(m) {
+       return m.score > 8;
+    })
 }
-greet('maria')
+console.log(greatMovies(movies))

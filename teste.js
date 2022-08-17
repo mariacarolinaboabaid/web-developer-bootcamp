82b431d79a328865136215ba90a293172f720be5
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-08-16 22:42:16 -0300

    Create Section 24

--- teste.js
+++ teste.js
@@ -1,11 +1,3 @@
-const movies = [{title: "Harry Potter", score: 10},
-{title: "Titanic", score: 9.5},
-{title: "Procurando Nemo", score: 8},
-{title: "50 Tons de Cinza", score: 4}]
-
-const goodMovies = movies.filter(function (movie){
-    return movie.score > 8;
-}).map(function (m){
-    m.title})
-
-console.log(goodMovies)
+const movies = {title: "Harry Potter", score: 10}
+const {title: name} = movies
+console.log(name)

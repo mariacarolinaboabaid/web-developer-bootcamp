b8688a9ff8701479865c447245509a5eae42d67c
Maria Carolina Boabaid <carolkboabaid@gmail.com>
2022-08-23 16:51:03 -0300

    Create Section 26

---
+++ Section 26/app.js
@@ -0,0 +1,70 @@
+// SELECTING THE BUTTON ELEMENTS
+const p1Button = document.getElementById("btnP1")
+const p2Button = document.getElementById("btnP2")
+const resetButton = document.getElementById("btnReset")
+// SELECTING THE SCORE KEEPER
+const countScoreP1 = document.getElementById("displayP1")
+const countScoreP2 = document.getElementById("displayP2")
+let winningScore = document.getElementById("playTo")
+const whoWin = document.getElementById("winner")
+// DECLARING VARIABLES
+let p1Score = 0
+let p2Score = 0
+let isGameOver = false
+// FUNCTION RESET 
+function reset() {
+    undisable()
+    p1Score = 0
+    p2Score = 0
+    countScoreP1.innerText = p1Score
+    countScoreP2.innerText = p2Score
+    countScoreP1.style.color = "black"
+    countScoreP2.style.color = "black"
+    whoWin.innerHTML = "<i>Use the buttons below to keep score</i>"
+    whoWin.style.color= "black"
+    isGameOver = false
+}
+function disable (){
+    p1Button.disabled = true
+    p2Button.disabled = true
+}
+function undisable(){
+    p1Button.disabled = false
+    p2Button.disabled = false
+}
+// SELECT EVENT
+winningScore.addEventListener("change", function () {
+    winningScore = parseInt(winningScore.value)
+    reset()
+})
+// BUTTONS EVENTS
+p1Button.addEventListener("click", function () {
+    if (!isGameOver) {
+        p1Score += 1
+        countScoreP1.innerText = p1Score
+        if (p1Score === winningScore) {
+            disable()
+            countScoreP1.style.color = "green";
+            countScoreP2.style.color = "red";
+            whoWin.innerHTML = "<b>Player One is the winner!<b>"
+            whoWin.style.color = "green"
+            isGameOver = true
+        }
+    }
+})
+p2Button.addEventListener("click", function () {
+    if (!isGameOver) {
+        p2Score += 1
+        countScoreP2.innerText = p2Score
+        if (p2Score === winningScore) {
+            disable()
+            countScoreP2.style.color = "green";
+            countScoreP1.style.color = "red";
+            whoWin.innerHTML = "<b>Player Two is the winner!<b>"
+            whoWin.style.color = "green"
+            isGameOver = true
+        }
+    }
+})
+resetButton.addEventListener("click", reset)
+

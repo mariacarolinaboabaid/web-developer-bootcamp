b8b63106833c3ebf9b58cf2049869ad60d2993fc
Maria Carolina Boabaid <90108003+mariacarolinaboabaid@users.noreply.github.com>
2022-09-08 22:06:51 -0300

    Create Section 28

--- Section 27/Tv Show Search/app.js
+++ Section 27/Tv Show Search/app.js
@@ -3,11 +3,12 @@ const inputTvShow = document.getElementById("userTvShow")
 const imgContainer = document.getElementById("container")
 
 // REQUEST FUNCTION
-searchForm.addEventListener("submit", function (event) {
+searchForm.addEventListener("submit", async function (event) {
     event.preventDefault()
     removeAllChild(imgContainer)
-    setTimeout(findingImg(), 2000)
-    displayImgShows(findingImg)
+    const nameTvShow = inputTvShow.value
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${nameTvShow}`)
+    displayImgShows(res)
     searchForm.reset()
 })
 
@@ -22,19 +23,11 @@ const displayImgShows = (resultShows) => {
         }
     }
 }
-const findingImg = async () => {
-    try {
-        const nameTvShow = inputTvShow.value
-        const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${nameTvShow}`)
-        return res
-    } catch {
-        console.log("Error")
-}
-}
 
 // REMOVING IMAGES FOR A NEW SUBMIT FUNCTION
 const removeAllChild = (parent) => {
-    for (let y = 0; y < parent.childElementCount; y++) {
-        parent.removeChild(parent.children[y])
+    while (parent.childElementCount !== 0) {
+        parent.removeChild(parent.firstChild)
     }
 }
+

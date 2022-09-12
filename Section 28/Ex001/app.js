85a1b1927b861889910272bcad1a968807a24548
Maria Carolina Boabaid <90108003+mariacarolinaboabaid@users.noreply.github.com>
2022-09-12 15:21:59 -0300

    Update

--- Section 28/Ex001/app.js
+++ Section 28/Ex001/app.js
@@ -1,20 +1,12 @@
+const colors = ["purple", "blue", "green", "grey", "yellow", "pink", "black", "teal", "olive", "magenta"]
 const div = document.querySelector("div")
 
-// FACTORY FUNCTION
-function makeColor(r, g, b){
-    const color = {}
-    color.r = r
-    color.g = g
-    color.b = b
-    color.rgb = function (){
-        return `rgb(${this.r}, ${this.g}, ${this.b})`
+function changingColor(){
+    for (let i = 0; i < colors.length; i++){
+        setTimeout(()=>{
+            document.body.style.backgroundColor = colors[i];
+        }, i * 400)
     }
-    return color
 }
-const firstColor = makeColor(155, 233, 122).rgb()
-console.log(firstColor)
 
-
-div.addEventListener("click", ()=>{
-    div.style.backgroundColor = firstColor
-})
+div.addEventListener("click", changingColor)
